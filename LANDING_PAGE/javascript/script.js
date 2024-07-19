e5f6be8008f9465772eb5bf574de1e9c2d99d7fb
manishkumar632 <manishmu632@gmail.com>
2024-07-19 21:24:50 +0530

    updated

--- LANDING_PAGE/javascript/script.js
+++ LANDING_PAGE/javascript/script.js
@@ -22,6 +22,32 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(slide, 1000);
 });
 
+function createCard(id, person) {
+    const card = document.createElement("div");
+    card.className = "testimonials-card";
+    const circle = document.createElement("div");
+    circle.className = "testimonials-card-circle";
+    const text = document.createElement("h1");
+    text.textContent = "⁕";
+    circle.appendChild(text);
+    card.appendChild(circle);
+    const content = document.createElement("div");
+    content.className = "testimonials-card-content";
+    return card;
+}
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const container = document.getElementsByClassName("testimonials-cards");
+        for (const id in data) {
+            if (data.hasOwnProperty(id)) {
+                const person = data[id];
+                const card = createCard(id, person);
+                container.appendChild(card);
+            }
+        }
+    })
+    .catch((error) => console.error("Error fetching JSON data:", error));
 
 const cards = document.querySelectorAll(".testimonials-card-description");
 cards.forEach((card) => {
