bf2fb384502f13206704964e20fb8e3170bad735
manishkumar632 <manishmu632@gmail.com>
2024-07-25 22:57:58 +0530

    updated

--- PORTFOLIO/tailwind.config.js
+++ PORTFOLIO/tailwind.config.js
@@ -10,9 +10,16 @@ module.exports = {
                 color1: "#070D1B",
                 color2: "#FEC544",
                 color3: "#CECEED"
+            },
+            screens: {
+                min4: { 'min': '400px' },
+                max4: { 'max': '400px' },
+                min14: { 'min': '1440px' },
+                max14: { 'max': '1440px' },
+                min7: {'min': '700px'},
+                max7: {'max': '700px'},
             }
         }
     },
     plugins: []
 };
-
