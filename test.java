f121b82c22afa3ac2323e47aa214de97e883e854
manishkumar632 <manishmu632@gmail.com>
2024-07-26 18:56:12 +0530

    updated

--- test.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-class test{
-    public static void main(String[] args) {
-        int[] arr = { 1, 2, 2, 2, 1, 1 };
-        ArrayList<Integer> list = new ArrayList<>();
-    }
-}
